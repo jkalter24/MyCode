c37c6a25f648151f85077e5fa701d7ae8f2e2948
jkalter24 <70232140+jkalter24@users.noreply.github.com>
2023-01-25 18:44:40 -0500

    Create W10.bat

---
+++ W10.bat
@@ -0,0 +1,46 @@
+# Create a system restore point
+Checkpoint-Computer -Description "Debloat Windows 10 Backup"
+
+# Create a backup of the current system
+$date = Get-Date -Format yyyy-MM-dd
+Backup-Computer -Description "Debloat Windows 10 Backup" -Path "C:\Windows10DebloatBackup_$date"
+
+# Remove Windows 10 apps
+Get-AppxPackage -AllUsers | Remove-AppxPackage
+
+# Remove OneDrive
+Remove-Item -Path "$env:SystemDrive\Users\$env:Username\OneDrive" -Recurse -Force
+Remove-Item -Path "$env:SystemDrive\ProgramData\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force
+
+# Remove Cortana
+Remove-WindowsPackage -PackageName Microsoft.549981C3F5F10
+
+# Remove Edge
+Remove-WindowsPackage -PackageName Microsoft.MicrosoftEdge
+
+# Remove Xbox App
+Remove-WindowsPackage -PackageName Microsoft.XboxApp
+
+# Remove Windows Store
+Remove-WindowsPackage -PackageName Microsoft.WindowsStore
+
+# Remove Windows Feedback
+Remove-WindowsPackage -PackageName Windows Feedback
+
+# Remove Windows Maps
+Remove-WindowsPackage -PackageName Microsoft.WindowsMaps
+
+# Optimize Ethernet connection
+$NIC = Get-NetAdapter | where {$_.Status -eq "Up"}
+$NIC | Disable-NetAdapter
+Start-Sleep -s 10
+$NIC | Enable-NetAdapter
+$NIC | Set-NetAdapterAdvancedProperty -RegistryKeyword "*SpeedDuplex" -RegistryValue "0x10000"
+
+# Disable Windows Telemetry
+$telemetry = New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Force
+New-ItemProperty -Path $telemetry.PSPath -Name "AllowTelemetry" -Value "0" -PropertyType "DWord"
+
+# Enable Dark mode
+$darkmode = New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Force
+New-ItemProperty -Path $darkmode.PSPath -Name "AppsUseLightTheme" -Value "0" -PropertyType "DWord"
